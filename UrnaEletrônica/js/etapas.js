c0b5169692c562480aa08e55c04c8aaa7f2cde0d
WILLIAN ELIDIO <willianelidio19@gmail.com>
2022-11-18 22:25:46 -0300

    Add files via upload

---
+++ UrnaEletrônica/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: '38111',
+              nome: 'LIGHT YAGAME',
+              partido: 'KIRA',
+              fotos:[
+                  {url:'38111.png', legenda: 'Vereador'}
+              ]
+          },
+          {
+              numero: '77222',
+              nome: 'RYUZAKY RYUGA',
+              partido: 'L',
+              fotos:[
+                  {url:'77222.png', legenda: 'Vereador'}
+              ]
+          },
+      ]
+  },
+  {
+      titulo: 'PREFEITO',
+      numeros: 2,
+      candidatos: [
+          {
+              numero: '99',
+              nome: 'SON GOKU',
+              partido: 'DBZ',
+              vice: 'VEGETA',
+              fotos:[
+                  {url:'99.png', legenda: 'Prefeito'},
+                  {url:'99_2.png', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+          {
+              numero: '84',
+              nome: 'NARUTO',
+              partido: 'KUN',
+              vice: 'SASUKE',
+              fotos:[
+                  {url:'84.png', legenda: 'Prefeito'},
+                  {url:'84_2.png', legenda: 'Vice-Prefeito', small: true}
+              ]
+          },
+      ]
+  }
+];
